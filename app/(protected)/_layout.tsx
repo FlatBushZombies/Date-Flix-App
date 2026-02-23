bf57368f6bcea6d0f04befcf6620c07d01c6516e
FlatBushZombies <brianmakanjira18@gmail.com>
2026-02-23 19:37:58 +0200

    add animated onboarding

---
+++ app/(protected)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+const Layout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="onboarding" />
+      <Stack.Screen name="post-auth" />
+    </Stack>
+  );
+};
+
+export default Layout;
